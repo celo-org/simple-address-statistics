bec60ff9870573d565d238f4f1f972336591b890
Aaron Boyd <aaronmboyd@gmail.com>
2021-01-29 16:31:16 +0100

    [STYLE] - Prettier

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function readCsv(filename: string): Promise<any> {
-  
   const stream = fs.createReadStream(filename);
-  const addresses = await neatCsv(stream); 
+  const addresses = await neatCsv(stream);
 
   // Skip header row
-  for(let i=0; i<addresses.length;i++) {
+  for (let i = 0; i < addresses.length; i++) {
     if (!isValidAddress(addresses[i].address))
       throw `${addresses[i].address} is not a valid Celo address`;
-    else
-      addresses[i].address = toCheckSumAddress(addresses[i].address);
-  };  
+    else addresses[i].address = toCheckSumAddress(addresses[i].address);
+  }
 
   console.log(`Addresses ${JSON.stringify(addresses)}`);
   return addresses;
@@ -34,30 +32,27 @@ async function start(
   toDate: string,
   outputFile?: string
 ): Promise<void> {
-
   const zero = new BigNumber(0);
-  
+
   console.log(`Input file is ${inputFile}`);
   console.log(`Output file is ${outputFile}`);
 
   // Parse dates
   let epochFromDate, epochToDate;
-  try{
+  try {
     epochFromDate = toEpochTimestamp(fromDate);
     epochToDate = toEpochTimestamp(toDate);
     console.log(`Date from is ${fromDate} (${epochFromDate})`);
     console.log(`Date from is ${toDate} (${epochToDate})`);
-  }
-  catch(err){
+  } catch (err) {
     console.error("Error parsing dates:" + err);
     throw err;
   }
 
   // Read file
   let addresses;
-  try {    
+  try {
     addresses = await readCsv(inputFile);
-    
   } catch (err) {
     console.error("Error reading CSV file:" + err);
     throw err;
@@ -65,93 +60,111 @@ async function start(
 
   // Get data
   const results = [];
-  try{
+  try {
     const cache = new InMemoryCache();
     const client = new ApolloClient({
       uri: process.env.GRAPHQL_ENDPOINT,
       cache: cache,
       name: "celo-simple-address-monitor",
       version: "0.0.1",
-      queryDeduplication: false
+      queryDeduplication: false,
     });
 
-    for(let i = 0; i < addresses.length;i++){
-      
+    for (let i = 0; i < addresses.length; i++) {
       const element = addresses[i];
       console.log("");
       console.log(`******************************************************`);
       console.log(`Started data retrieval for ${JSON.stringify(element)}`);
 
-      const variables = { address: element.address, token: process.env.TOKEN, localCurrencyCode: process.env.LOCAL_CURRENCY_CODE }
-      const options : QueryOptions = {
+      const variables = {
+        address: element.address,
+        token: process.env.TOKEN,
+        localCurrencyCode: process.env.LOCAL_CURRENCY_CODE,
+      };
+      const options: QueryOptions = {
         errorPolicy: "all",
         fetchPolicy: "no-cache",
         query: ADDRESS_QUERY,
-        variables: variables   
-      }
-      const response = await client.query(options);  
+        variables: variables,
+      };
+      const response = await client.query(options);
       const data = response.data;
       console.log(`Completed data retreival for ${addresses[i].address}`);
-      
+
       const totals = {
-        totalReceived : zero,
+        totalReceived: zero,
         receivedCount: zero,
-        averageReceived : zero,
-        totalSent : zero,
+        averageReceived: zero,
+        totalSent: zero,
         sentCount: zero,
-        averageSent : zero,
+        averageSent: zero,
         totalFees: zero,
         feesCount: zero,
         averageFees: zero,
-        averageTransferTime: zero
-      }
+        averageTransferTime: zero,
+      };
 
-      for(let j = 0; j<data?.tokenTransactions?.edges.length; j++) {        
-        
+      for (let j = 0; j < data?.tokenTransactions?.edges.length; j++) {
         const element = data.tokenTransactions.edges[j].node;
-        
+
         // Skip if this node doesn't match the time range
-        if(!(element.timestamp >= epochFromDate && element.timestamp < epochToDate )){
-          console.log(`Skipping txId ${element.hash} timestamp ${element.timestamp} is out of range ${epochFromDate} to ${epochToDate}`);
+        if (
+          !(
+            element.timestamp >= epochFromDate &&
+            element.timestamp < epochToDate
+          )
+        ) {
+          console.log(
+            `Skipping txId ${element.hash} timestamp ${element.timestamp} is out of range ${epochFromDate} to ${epochToDate}`
+          );
           continue;
-        }
-        else{
+        } else {
           console.log(`Processing txId ${element.hash} `);
         }
 
-        if(element.type === "SENT" || element.type === "EXCHANGE"){          
-          totals.totalSent = totals.totalSent.plus(new BigNumber(element.amount.localAmount.value));
+        if (element.type === "SENT" || element.type === "EXCHANGE") {
+          totals.totalSent = totals.totalSent.plus(
+            new BigNumber(element.amount.localAmount.value)
+          );
           totals.sentCount = totals.sentCount.plus(new BigNumber(1));
-          
-          for(let k = 0; k < element.fees.length; k++){
-            totals.totalFees = totals.totalFees.plus(new BigNumber(element.fees[k].amount.localAmount.value));
+
+          for (let k = 0; k < element.fees.length; k++) {
+            totals.totalFees = totals.totalFees.plus(
+              new BigNumber(element.fees[k].amount.localAmount.value)
+            );
             totals.feesCount = totals.feesCount.plus(new BigNumber(1));
           }
-        }
-        else if (element.type === "RECEIVED") {
-          totals.totalReceived = totals.totalReceived.plus(new BigNumber(element.amount.localAmount.value));
+        } else if (element.type === "RECEIVED") {
+          totals.totalReceived = totals.totalReceived.plus(
+            new BigNumber(element.amount.localAmount.value)
+          );
           totals.receivedCount = totals.receivedCount.plus(new BigNumber(1));
         }
-      };
+      }
 
       // Calculate totals
-      totals.averageSent = !totals.sentCount.isEqualTo(zero) ? totals.totalSent.dividedBy(totals.sentCount) : zero;
-      totals.averageReceived = !totals.receivedCount.isEqualTo(zero) ? totals.totalReceived.dividedBy(totals.receivedCount): zero;
-      totals.averageFees = !totals.feesCount.isEqualTo(zero) ? totals.totalFees.dividedBy(totals.feesCount) : zero;
+      totals.averageSent = !totals.sentCount.isEqualTo(zero)
+        ? totals.totalSent.dividedBy(totals.sentCount)
+        : zero;
+      totals.averageReceived = !totals.receivedCount.isEqualTo(zero)
+        ? totals.totalReceived.dividedBy(totals.receivedCount)
+        : zero;
+      totals.averageFees = !totals.feesCount.isEqualTo(zero)
+        ? totals.totalFees.dividedBy(totals.feesCount)
+        : zero;
 
       const stringTotals = {
-        totalReceived : totals.totalReceived.toString(),
-        averageReceived :  totals.averageReceived.toString(),
-        totalSent :  totals.totalSent.toString(),
-        averageSent :  totals.averageSent.toString(),
-        totalFees:  totals.totalFees.toString(),
-        averageFees:  totals.averageFees.toString(),
-        averageTransferTime:  totals.averageTransferTime.toString()
-      }
-      results.push({ address: addresses[i].address, ...stringTotals});
-    };
-  }
-  catch(err){
+        totalReceived: totals.totalReceived.toString(),
+        averageReceived: totals.averageReceived.toString(),
+        totalSent: totals.totalSent.toString(),
+        averageSent: totals.averageSent.toString(),
+        totalFees: totals.totalFees.toString(),
+        averageFees: totals.averageFees.toString(),
+        averageTransferTime: totals.averageTransferTime.toString(),
+      };
+      results.push({ address: addresses[i].address, ...stringTotals });
+    }
+  } catch (err) {
     console.error("Error retrieving data:" + err);
     throw err;
   }
@@ -161,13 +174,12 @@ async function start(
   console.table(results);
 
   // Output to file
-  if(outputFile != undefined){    
-    console.log("")
+  if (outputFile != undefined) {
+    console.log("");
     const ws = fs.createWriteStream(outputFile);
     fastcsv.write(results, { headers: true }).pipe(ws);
     console.log(`Output written to ${outputFile}. Ending.`);
-  }
-  else{
+  } else {
     console.log("No output file specified. Ending.");
   }
 }
--- src/lib/gql.ts
+++ src/lib/gql.ts
@@ -1,9 +1,17 @@
-import {gql} from "@apollo/client/core";
+import { gql } from "@apollo/client/core";
 
 export const ADDRESS_QUERY = gql`
-# Write your query or mutation here
-query UserTransactions($address: Address!, $token: Token!, $localCurrencyCode: String) {
-    tokenTransactions(address: $address, token: $token, localCurrencyCode: $localCurrencyCode) {
+  # Write your query or mutation here
+  query UserTransactions(
+    $address: Address!
+    $token: Token!
+    $localCurrencyCode: String
+  ) {
+    tokenTransactions(
+      address: $address
+      token: $token
+      localCurrencyCode: $localCurrencyCode
+    ) {
       edges {
         node {
           ...TransactionFeed
@@ -11,46 +19,17 @@ query UserTransactions($address: Address!, $token: Token!, $localCurrencyCode: S
       }
     }
   }
-fragment TransactionFeed on TokenTransaction {
-  ...ExchangeItem
-  ...TransferItem
-}
-fragment ExchangeItem on TokenExchange {
-      __typename
+  fragment TransactionFeed on TokenTransaction {
+    ...ExchangeItem
+    ...TransferItem
+  }
+  fragment ExchangeItem on TokenExchange {
+    __typename
+    type
+    hash
+    fees {
       type
-      hash
-      fees {
-        type
-        amount {
-          value
-          currencyCode
-          localAmount {
-            value
-            currencyCode
-            exchangeRate
-          }
-        }
-      }
       amount {
-        value
-        currencyCode
-        localAmount {
-          value
-          currencyCode
-          exchangeRate
-        }
-      }
-      timestamp
-      takerAmount {
-        value
-        currencyCode
-        localAmount {
-          value
-          currencyCode
-          exchangeRate
-        }
-      }
-      makerAmount {
         value
         currencyCode
         localAmount {
@@ -60,22 +39,40 @@ fragment ExchangeItem on TokenExchange {
         }
       }
     }
-    fragment TransferItem on TokenTransfer {
-      __typename
-      type
-      fees {
-        type
-        amount {
-          value
-          currencyCode
-          localAmount {
-            value
-            currencyCode
-            exchangeRate
-          }
-        }
+    amount {
+      value
+      currencyCode
+      localAmount {
+        value
+        currencyCode
+        exchangeRate
       }
-      hash
+    }
+    timestamp
+    takerAmount {
+      value
+      currencyCode
+      localAmount {
+        value
+        currencyCode
+        exchangeRate
+      }
+    }
+    makerAmount {
+      value
+      currencyCode
+      localAmount {
+        value
+        currencyCode
+        exchangeRate
+      }
+    }
+  }
+  fragment TransferItem on TokenTransfer {
+    __typename
+    type
+    fees {
+      type
       amount {
         value
         currencyCode
@@ -85,9 +82,20 @@ fragment ExchangeItem on TokenExchange {
           exchangeRate
         }
       }
-      timestamp
-      address
+    }
+    hash
+    amount {
+      value
+      currencyCode
+      localAmount {
+        value
+        currencyCode
+        exchangeRate
+      }
+    }
+    timestamp
+    address
     #   account
-      comment
-    }
+    comment
+  }
 `;
